76e118310ffeb791bb5a0e5aa58b2ad2a82ee489
user <user@DESKTOP-N72ADEG>
2026-03-24 21:11:45 +0200

    Add project files.

---
+++ ארז רינגלר/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ארז_רינגלר.Models;
+
+namespace ארז_רינגלר.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult AreaOfTreatment()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+    
+}
---
+++ ארז רינגלר/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ארז_רינגלר.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
